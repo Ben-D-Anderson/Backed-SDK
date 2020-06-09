2c258d6f1963e5437fed728fa7456eaf6f1e8080
Ben-D-Anderson <benanderson.david@gmail.com>
2020-06-09 20:56:27 +0100

    Updated files response to use directory and file classes

---
+++ src/backed/sdk/response/FilesResponse.java
@@ -0,0 +1,21 @@
+package backed.sdk.response;
+
+import backed.sdk.obj.Directory;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+public class FilesResponse extends Response {
+
+    private Directory rootDirectory;
+
+    public FilesResponse(JsonElement jsonElement) {
+        super(jsonElement);
+        JsonArray filesJsonArray = jsonElement.getAsJsonObject().getAsJsonArray("files");
+        rootDirectory = new Directory("", "", filesJsonArray, null);
+    }
+
+    public Directory getRootDirectory() {
+        return rootDirectory;
+    }
+
+}
